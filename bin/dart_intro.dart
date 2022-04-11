cb76b401112716aff3e41dbe4992d6187944e158
skedaddl3 <hexisdylan23@gmail.com>
2022-04-11 17:45:46 +0800

    Initial Commit

--- bin/dart_intro.dart
+++ bin/dart_intro.dart
@@ -1,3 +1,38 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+/*
+  To sum up what i've learned at Section 2 of the
+  Dart and Flutter: The Complete Developer's Guide
+  I implemented Object Oriented Programming in Dart
+*/
+
+void main() {
+  var person = new Person('Dids Irwyn', 'Tome', 'Reyes');
+
+  person.printInfo();
+  person.address = 'Caloocan City, Philippines';
+  person.dispAddress(); // Call Function to Display Address
+  person.age = 21;
+
+  print(person.dispAge()); // Another method to Display a return value
+}
+
+class Person {
+  late String firstName, middleInitial, lastName, address;
+  late int age;
+
+  Person(this.firstName, this.middleInitial, this.lastName);
+
+  // Dynamic function allows non-null value
+  dispAddress() {
+    print('From ' + address);
+  }
+
+  dispAge() {
+    // since Dart doesn't support multiple return values
+    // I returned an array :D
+    return [age, "years old"];
+  }
+
+  printInfo() {
+    print('My name is ' + firstName + ' ' + middleInitial + ' ' + lastName);
+  }
 }
